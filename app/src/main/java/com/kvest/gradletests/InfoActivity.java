19054ffbe7764ff77842dac7b2c0075a1b644d24
Roman Bielokon <roman_slavyansk@mail.ru>
2015-03-12 22:44:34 +0200

    initial commit

---
+++ app/src/main/java/com/kvest/gradletests/InfoActivity.java
@@ -0,0 +1,108 @@
+package com.kvest.gradletests;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
+import java.util.Iterator;
+import java.util.List;
+
+
+public class InfoActivity extends ActionBarActivity {
+    private TextView sh1;
+    private TextView debug;
+    private TextView permissions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+        sh1 = (TextView) findViewById(R.id.sha1);
+        debug = (TextView) findViewById(R.id.debug);
+        permissions = (TextView) findViewById(R.id.permissions);
+
+        sh1.setText(getCertificateSHA1Fingerprint());
+        debug.setText("BuildConfig.DEBUG=" + BuildConfig.DEBUG + ", BuildConfig.FLAVOR=" + BuildConfig.FLAVOR + ", BuildConfig.BUILD_TYPE" + BuildConfig.BUILD_TYPE);
+
+
+        try {
+            StringBuilder sb = new StringBuilder();
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_PERMISSIONS);
+            if (info.requestedPermissions != null) {
+                for (String permission : info.requestedPermissions) {
+                    sb.append(permission).append("\n");
+                }
+            }
+            permissions.setText(sb.toString());
+        } catch (PackageManager.NameNotFoundException e) {
+            permissions.setText("ERROR");
+            e.printStackTrace();
+        }
+
+    }
+
+    private String getCertificateSHA1Fingerprint() {
+        PackageManager pm = getPackageManager();
+        String packageName = getPackageName();
+        int flags = PackageManager.GET_SIGNATURES;
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = pm.getPackageInfo(packageName, flags);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        Signature[] signatures = packageInfo.signatures;
+        byte[] cert = signatures[0].toByteArray();
+        InputStream input = new ByteArrayInputStream(cert);
+        CertificateFactory cf = null;
+        try {
+            cf = CertificateFactory.getInstance("X509");
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        }
+        X509Certificate c = null;
+        try {
+            c = (X509Certificate) cf.generateCertificate(input);
+        } catch (CertificateException e) {
+            e.printStackTrace();
+        }
+        String hexString = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA1");
+            byte[] publicKey = md.digest(c.getEncoded());
+            hexString = byte2HexFormatted(publicKey);
+        } catch (NoSuchAlgorithmException e1) {
+            e1.printStackTrace();
+        } catch (CertificateEncodingException e) {
+            e.printStackTrace();
+        }
+        return hexString;
+    }
+
+    public static String byte2HexFormatted(byte[] arr) {
+        StringBuilder str = new StringBuilder(arr.length * 2);
+        for (int i = 0; i < arr.length; i++) {
+            String h = Integer.toHexString(arr[i]);
+            int l = h.length();
+            if (l == 1) h = "0" + h;
+            if (l > 2) h = h.substring(l - 2, l);
+            str.append(h.toUpperCase());
+            if (i < (arr.length - 1)) str.append(':');
+        }
+        return str.toString();
+    }
+}
